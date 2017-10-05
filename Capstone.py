8fbaae6198795cf683a39f93ec600e3f76a7c870
Jordan McCaskill <mccaskillj2@mymacewan.ca>
2017-10-04 19:25:24 -0600

    Create the base for the script

---
+++ Capstone.py
@@ -0,0 +1,53 @@
+from sys import *
+from PIL import Image
+from traceTop import *
+
+def main(argv):
+    if len(argv) != 2:
+        print("usage:",argv[0], "filename")
+        return 0;
+    try:
+        Image.open(argv[1])
+    except:
+        print("file \"" + argv[1] + "\" not found")
+        return 0
+
+    menu()
+
+
+def menu():
+    val = menuItems()
+    while checkChoice(val):
+        print()
+        val = menuItems()
+
+    val = int(val)
+
+    if val == 1:
+        return 0
+    elif val == 2:
+        return 0
+    elif val == 3:
+        traceTop(argv[1])
+    else:
+        return 0
+
+
+def menuItems():
+    print("Please Select One of the Following:")
+    print("\t1) Vertical Hist")
+    print("\t2) Horizontal Hist")
+    print("\t3) Trace Top")
+    return str(input("Choice: "))
+
+def checkChoice(userVal):
+    if not userVal.isdigit():
+        return True
+    else:
+        val = int(userVal)
+        if 0 >= val > 3:
+            return True
+
+    return False
+
+main(argv)
--- traceTop.py
+++ traceTop.py
@@ -1,28 +1,33 @@
 from PIL import Image
 
-def findFrontRear():
-    pic = Image.open("Images/hello.png")
 
+def traceTop(filename):
+    pic = Image.open(filename)
+
+    print(findFrontRear(pic))
+
+
+def findFrontRear(pic):
     pos = []
 
     for i in range(pic.size[0]):
         found = False
         for j in range(pic.size[1]):
-            if pic.getpixel((i,j)) < 100:
-                found = True
+            if pic.getpixel((i, j)) < 100:
+                found = (i,j)
                 break
         pos.append(found)
 
-    front = 0
+    front = (0, 0)
     for i in range(len(pos)):
         if pos[i]:
-            front = i
+            front = pos[i]
             break
 
-    rear = pic.size[0]
-    for i in range(len(pos)-1,-1,-1):
+    rear = (0, 0)
+    for i in range(len(pos)-1, -1, -1):
         if pos[i]:
-            rear = i
+            rear = pos[i]
             break
 
     return front, rear
